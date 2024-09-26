334483ec67dfa36c413544da74cbc973ef6d70d0
CarolinaMidori <cacsmidori@gmail.com>
2024-09-26 16:20:00 -0300

    Script e Código Calculadora de IMC
    Script para aplicação da Calculadora de IMC

---
+++ calculadora_de_imc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Iniciando o Script"
+
+echo "O diretório atual é $PWD"
+
+
+sudo apt update
+sudo apt install python3
+
+python3 /home/midori/modulo1/linux/pratica/Exercicio_Modulo1.py
+
+
+echo "Finalizando o Script"
+
